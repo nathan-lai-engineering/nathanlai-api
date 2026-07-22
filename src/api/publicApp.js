f81f427c746212adcc84a981ef90f30c3407b726
--global <nathanlai.software@gmail.com>
2026-07-22 12:38:53 -0700

    swagger

--- src/api/publicApp.js
+++ src/api/publicApp.js
@@ -3,8 +3,25 @@
  * exposed to outside network
  * requires rate limiting + cloudflare security
  */
-
+import Fastify from 'fastify';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
 
 export function buildPublicApp(){
+    const app = Fastify({ logger: true });
 
+    app.register(swagger, {
+        openapi: {
+        info: {
+            title: 'Nathan Lai public API endpoints',
+            version: '1.0.0',
+        },
+        },
+    });
+
+        app.register(swaggerUi, {
+        routePrefix: '/docs',
+    });
+
+    return app;
 }
