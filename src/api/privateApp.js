e2b3a463282bb252611d42b9b5a1200ae486bfb9
--global <nathanlai.software@gmail.com>
2026-08-08 13:51:48 -0700

    swagger for private api; requires api tunnel found in infra/devtools

--- src/api/privateApp.js
+++ src/api/privateApp.js
@@ -5,11 +5,26 @@
  */
 
 import Fastify from 'fastify';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
 import actionLogsRoutes from './routes/private/actionLogs.js';
 
 export function buildPrivateApp() {
 
   const app = Fastify({ logger: true });
+
+  app.register(swagger, {
+    openapi: {
+      info: {
+        title: 'Nathan Lai private API endpoints',
+        version: '1.0.0',
+      },
+    },
+  });
+
+  app.register(swaggerUi, {
+    routePrefix: '/docs',
+  });
 
   app.register(actionLogsRoutes);
 
